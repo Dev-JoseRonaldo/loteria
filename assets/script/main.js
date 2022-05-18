da283104d514d8b48bd5da66d6974084c466035b
José Ronaldo <dev.joseronaldo@gmail.com>
2022-05-18 16:45:20 -0300

    :sparkles: feat: adiacionando data e id do sorteio dinamicamente atravês da api

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,7 +3,6 @@ const url = 'https://brainn-api-loterias.herokuapp.com/api/v1'
 const select = document.querySelector('.select__sena')
 const numerosbox = document.querySelector('.left__numbers')
 const logo = document.querySelector('.logo-center__title')
-
 const textDesktop = document.querySelector('.text-desktop__span')
 const textMobile = document.querySelector('.text-down__text-mobile')
 
@@ -12,7 +11,6 @@ function getLoterias() {
   axios
     .get(`${url}/loterias`)
     .then(response => {
-      console.log(response.data)
       let loterias = response.data
       addLoteriasInSelect(loterias)
     })
@@ -27,7 +25,6 @@ function addLoteriasInSelect(loterias) {
     option.value = `${loterias[i].nome}`
     option.classList.add('sena__item')
     select.add(option)
-    //select.innerHTML += `<option class="sena__item" value="${loterias[i].nome}">${loterias[i].nome}</option>`
   })
 }
 
@@ -45,6 +42,12 @@ function changeLogo(nameLoteria) {
   logo.textContent = nameLoteria
 }
 
+// função responsável por mudar o nome do logo de acordo com o <select>
+function changeIdConcurso(idLoteria, date) {
+  textDesktop.textContent = `${idLoteria} - ${date}`
+  textMobile.textContent = `concurso N° ${idLoteria}`
+}
+
 // função responsável por obter o ID da loteria selecionada no <select>
 function getIdConcurso(indexIdConcurso) {
   axios
@@ -56,11 +59,24 @@ function getIdConcurso(indexIdConcurso) {
     .catch(error => console.error(error))
 }
 
+// função responsável em tranformar a data bruta da api no padrão "dia/mes/ano"
+function getdate(dateFull) {
+  let ano = dateFull.data.slice(0, 4)
+  let mes = dateFull.data.slice(5, 7)
+  let dia = dateFull.data.slice(8, 10)
+
+  let date = `${dia}/${mes}/${ano}`
+  return date
+}
+
+// função que pega: id da lotérica, index da lotéria, numeros sorteados, e a data do sorteio.
 function getNumerosSorteados(id) {
   axios
     .get(`${url}/concursos/${id}`)
     .then(response => {
       addNumerosSorteados(response.data.numeros)
+
+      changeIdConcurso(id, getdate(response.data))
     })
     .catch(error => console.error(error))
 }
@@ -76,6 +92,7 @@ function addNumerosSorteados(numeros) {
     `
   })
 }
+
 //função reponsável por chamar as outras funções
 function main() {
   getLoterias()
